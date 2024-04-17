446d139c96aafe33b22ddcb1e8669b652dd5ca7b
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2024-04-17 11:59:34 +1000

    added functions and corresponding tests

--- util.js
+++ util.js
@@ -1,3 +1,58 @@
-function capitalise() {}
+function capitalise(str) {
+  return str.toUpperCase();
+}
 
-export { capitalise };
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+const calculator = {
+  add: (a, b) => {
+    return a + b;
+  },
+  subtract: (a, b) => {
+    return a - b;
+  },
+  divide: (a, b) => {
+    return Math.round((a / b) * 100) / 100;
+  },
+  multiply: (a, b) => {
+    return a * b;
+  },
+};
+
+function doShift(code, lower, upper, shift) {
+  return String.fromCharCode(
+    code + shift <= upper
+      ? code + shift
+      : ((code + shift) % upper) + (lower - 1)
+  );
+}
+
+function caesarCipher(str, shift) {
+  let arr = str.split("");
+  for (let i = 0; i < arr.length; i++) {
+    let code = str.charCodeAt(i);
+
+    if (code >= 65 && code <= 90) {
+      arr[i] = doShift(code, 65, 90, shift);
+    } else if (code >= 97 && code <= 122) {
+      arr[i] = doShift(code, 97, 122, shift);
+    }
+    // console.log(arr[i]);
+  }
+  return arr.join("");
+}
+
+function analyzeArray(arr) {
+  const len = arr.length;
+  const avg = arr.reduce((prev, cur) => prev + cur, 0) / len;
+  return {
+    average: avg,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: len,
+  };
+}
+
+export { capitalise, reverseString, calculator, caesarCipher, analyzeArray };
--- util.test.js
+++ util.test.js
@@ -1,1 +1,58 @@
-import { capitalise } from "./util";
+import {
+  capitalise,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} from "./util";
+import { describe } from "node:test";
+
+const str1 = "abc";
+const str2 = "Hello World!";
+const str3 = "abc!.&*";
+
+test("capitalising a string", () => {
+  expect(capitalise(str1)).toBe("ABC");
+  expect(capitalise(str2)).toBe("HELLO WORLD!");
+});
+
+test("reversing a string", () => {
+  expect(reverseString(str1)).toBe("cba");
+  expect(reverseString(str2)).toBe("!dlroW olleH");
+});
+
+describe("calculator functions", () => {
+  const a = 123;
+  const b = 100;
+  test("adding two number", () => {
+    expect(calculator.add(a, b)).toEqual(223);
+  });
+  test("subtracting", () => {
+    expect(calculator.subtract(a, b)).toEqual(23);
+    expect(calculator.subtract(b, a)).toEqual(-23);
+  });
+  test("multiplying", () => {
+    expect(calculator.multiply(a, b)).toEqual(12300);
+  });
+  test("dividing", () => {
+    expect(calculator.divide(a, b)).toEqual(1.23);
+    expect(calculator.divide(b, a)).toEqual(0.81);
+  });
+});
+
+test("encoding a string with Ceasar cipher", () => {
+  const shift = 13;
+  expect(caesarCipher(str1, shift)).toBe("nop");
+  // expect(caesarCipher(capitalise(str1), shift)).toBe("NOP");
+  expect(caesarCipher(capitalise(str3), shift)).toBe("NOP!.&*");
+  expect(caesarCipher(str2, shift)).toBe("Uryyb Jbeyq!");
+});
+
+test("analysing an array", () => {
+  const array = [1, 8, 3, 4, 2, 6];
+  const obj = analyzeArray(array);
+  expect(obj.average).toEqual(4);
+  expect(obj.min).toEqual(1);
+  expect(obj.max).toEqual(8);
+  expect(obj.length).toEqual(6);
+});
